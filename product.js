dec675fd11de11f6ecc0e1dd5db89d0fd06b5381
zargo <jpkz@mail.ru>
2020-12-28 02:58:24 +0600

    pull-request

--- product.js
+++ product.js
@@ -1,15 +1,1 @@
-//Товары в корзине хранятся в массиве. Задачи:
-//a) Организовать такой массив для хранения товаров в корзине;
-//b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
-
-var product = [20, 40, 90];
-var service = [12, 22, 33];
-var abonPay = [225, 340, 550];
-function countBasketPrice(product, service, abonPay) {
-
-    return product + service + abonPay;
-
-}
-console.log(countBasketPrice(...product))
-console.log(countBasketPrice(...service))
-console.log(countBasketPrice(...abonPay))
+//pull request
